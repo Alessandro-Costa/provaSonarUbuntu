c284f84d1ea251e79d271fb79373409518cd1188
Alessandro-Costa <alessandro.costa.ing@gmail.com>
2021-03-23 16:48:15 +0100

    aggiungo la prova

---
+++ HelloWord.java
@@ -0,0 +1,9 @@
+package prova;  
+import java.util.logging.Logger; 
+import java.util.logging.Level;   
+public class Hello {          
+	static Logger logger = Logger.getLogger(Hello.class.getName());          
+public static void main(String[] args) {      
+	logger.log(Level.INFO,"Hello world");      
+	}
+}
